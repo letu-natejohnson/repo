ac84b1d0fd97b2abf7eb32ac6bd65caae64e1615
Nate Johnson <natejohnson@letu.edu>
2020-01-23 17:59:27 -0500

    Added a test hello world class to the repo.

---
+++ TestClass.java
@@ -0,0 +1,19 @@
+/**TestClass
+
+	testing the repo
+
+*/
+class TestClass{
+
+	//METHODS
+	/**Main method
+	@return 0
+	*/
+	public static int(String[] args) {
+
+		System.out.println("hello world~!");
+		return 0;
+
+	}
+
+}
